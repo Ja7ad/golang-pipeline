0c8ad8cc52471a21e4c09db1228e49af73ee24d4
javad <ja7adr@gmail.com>
2021-05-27 10:54:18 +0430

    Fix hello function return value

--- main_test.go
+++ main_test.go
@@ -6,6 +6,6 @@ func TestHello(t *testing.T) {
 	want := "Hello Golang"
 	got := hello()
 	if want != got {
-		t.Fatalf("want %s, got %s\n", want, got)
+		t.Fatalf("want  %s, got %s\n", want, got)
 	}
 }
